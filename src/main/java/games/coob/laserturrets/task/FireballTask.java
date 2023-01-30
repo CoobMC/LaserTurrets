27d77b7cdf54c29f9eeca1f87aa194da0b5a0a50
JackOUT <jack.bikar@gmail.com>
2023-01-30 19:12:12 +0100

    Improved the fireball turret

--- src/main/java/games/coob/laserturrets/task/FireballTask.java
+++ src/main/java/games/coob/laserturrets/task/FireballTask.java
@@ -6,8 +6,8 @@ import games.coob.laserturrets.util.EntityUtil;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Fireball;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.SmallFireball;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 import org.mineacademy.fo.Common;
@@ -23,7 +23,7 @@ public class FireballTask extends BukkitRunnable {
 				continue;
 
 			final Location location = turretData.getLocation();
-			final Location shootLocation = location.clone().add(0.5, 1.4, 0.5);
+			final Location shootLocation = location.clone().add(0.5, 1, 0.5);
 			final Block block = location.getBlock();
 			final int level = turretData.getCurrentLevel();
 			final int range = turretData.getLevel(level).getRange();
@@ -39,19 +39,24 @@ public class FireballTask extends BukkitRunnable {
 	private void shootFireball(final LivingEntity target, final Block block) {
 		if (target != null) {
 			final Location blockLocation = block.getLocation().clone().add(0.5, 1.4, 0.5);
-			final Location targetLocation = target.getEyeLocation().clone().add(0, -0.5, 0);
+			final Location targetLocation = target.getEyeLocation().clone();
 			final Vector vector = targetLocation.subtract(blockLocation).toVector().normalize();
-			final Fireball fireball = block.getWorld().spawn(blockLocation, Fireball.class);
+			final SmallFireball fireball = block.getWorld().spawn(blockLocation, SmallFireball.class);
 
-			fireball.setYield(1);
 			fireball.setDirection(vector);
+			fireball.setIsIncendiary(false);
 
-			for (int i = 0; i <= 10; i++)
-				if (fireball.getLocation().getBlock().getType() != Material.AIR)
-					fireball.teleport(fireball.getLocation().add(vector.clone().add(new Vector(0, -0.1, 0)).normalize().multiply(0.1)));
+			for (int i = 0; i <= 10; i++) {
+				if (fireball.getLocation().getBlock().getType() != Material.AIR) {
+					fireball.teleport(fireball.getLocation().add(vector.clone().normalize().multiply(0.08)));
+					System.out.println(block.getLocation().distance(target.getLocation()));
 
-			fireball.setVelocity(vector.add(new Vector(0, 0.02, 0)).multiply(1.4));
-			Common.runLater(80, fireball::remove);
+					if (block.getLocation().distance(target.getLocation()) < 1.6)
+						target.setVelocity(vector.clone().multiply(0.25));
+				}
+			}
+
+			Common.runLater(100, fireball::remove);
 		}
 	}
 }
